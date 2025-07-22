8bd85c7735789d4f412515e8d6dbd825ff84bbf4
umarovarslan <64959749+umarovarslan@users.noreply.github.com>
2025-07-22 21:51:25 +0800

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.routes.converter import converter_bp
+from routes.converter import converter_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
